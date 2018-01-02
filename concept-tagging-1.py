bb8266a4ca84b58fa25729ed48d4e1dc4a38e753
thomasferguson <32786757+thomasferguson@users.noreply.github.com>
2018-01-02 00:52:08 -0600

    Writes output as .csv for later reuse

--- concept-tagging-1.py
+++ concept-tagging-1.py
@@ -13,14 +13,23 @@ from gutenberg.cleanup import strip_headers
 
 from langdetect import detect
 
+import pandas as pd
+
 output_dict = {}
 
-for root, dirs, files in os.walk("E://gutenberg", topdown = False):
+for root, dirs, files in os.walk("E://gutenberg//1//0//1//4", topdown = False):
     for name in files:
         if name[-4:] == ".txt" and name[-6:] != "-8.txt" and name not in output_dict.keys():
             text_loc = os.path.join(root,name)
-            text1 = strip_headers(open(text_loc,"r").read())
-            if detect(text1) == "en":
-                text2 = Textatistic(text1)
-                output_dict[text_loc] = text2.fleschkincaid_score
-                print(text_loc, output_dict[text_loc])
+            try:
+                text0 = open(text_loc, "r").read()
+                text1 = strip_headers(open(text_loc,"r").read())
+                if detect(text1) == "en":
+                    text2 = Textatistic(text1)
+                    output_dict[text_loc] = text2.fleschkincaid_score
+                    print(text_loc, output_dict[text_loc])
+            except:
+                continue
+            
+output_dataframe = pd.DataFrame.from_dict(output_dict, orient = "index")
+output_dataframe.to_csv("output.csv")
